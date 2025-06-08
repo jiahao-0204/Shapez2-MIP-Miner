18319ba036897b8cba47fdcfab5d6e918d0189d9
Jiahao Wang <github@jiahao.wang>
2025-06-08 16:18:37 +0100

    fix(node_sink)

--- astroid_solver.py
+++ astroid_solver.py
@@ -43,9 +43,8 @@ class AstroidSolver:
         # list of sink nodes
         x_min_sink = 0
         x_max_sink = width
-        y_min_sink = height - 1
-        y_max_sink = height - 1
-        nodes_sink = [(x, y) for x in range(x_min_sink, x_max_sink + 1) for y in range(y_min_sink, y_max_sink + 1)]
+        y_sink = height - 1
+        nodes_sink = [(x, y_sink) for x in range(x_min_sink, x_max_sink)]
         
         # ----------------------------------------------------------
         # initialize the model
