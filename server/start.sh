b7d8ccf3643bb9a41ca458a414d49014c38cab79
Jiahao Wang <github@jiahao.wang>
2025-06-10 16:46:58 +0100

    feat(allow direct ip connection)

--- server/start.sh
+++ server/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
 conda activate shapez2
-exec uvicorn webapp:app --host 127.0.0.1 --port 8000 --reload
+exec uvicorn webapp:app --host 0.0.0.0 --port 8000 --reload
