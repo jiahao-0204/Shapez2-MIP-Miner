40a686fc0d74fc0c3b32bff3bfef3d4151153b73
Jiahao Wang <github@jiahao.wang>
2025-06-10 01:38:26 +0100

    feat(test using simple square)

--- astroid_solver.py
+++ astroid_solver.py
@@ -14,6 +14,7 @@ import numpy as np
 from astroid_parser import astroid_parser
 from var_to_txt import var_to_txt, FakeVar
 from blueprint_composer import compose_blueprint
+from astroid_parser import get_brush_blueprint, parse_using_blueprint
 
 DIRECTIONS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
@@ -550,9 +551,9 @@ def render_result(all_miner_platforms: List[FakeVar],
     return buffer
     
 if __name__ == "__main__":
-    # read input png
-    path = Path("images/example3.png")
-    astroid_location = astroid_parser(path, 0.5)    
+    # use simple coordinates
+    input_miner_blueprint = get_brush_blueprint(20)
+    astroid_location = parse_using_blueprint(input_miner_blueprint)
     
     # return if none
     if astroid_location is None:
@@ -563,8 +564,8 @@ if __name__ == "__main__":
     MINER_BLUEPRINT = "SHAPEZ2-2-H4sIAN0dd2cA/6xaXW+bMBT9L9Ye0YTNl0Haw7J2UrVEqtqs2jRVE0qcDo1C5JBtUZX/PhIMNRCofU37ELXhnHt8fX2uDbygBxRhbHsWmt2i6AW9Kw5bhiJ0s0vjbI0sdLPKs9MXV3ERo+gHSsq/o+rb2zResWeWFeVl53+n8SHfF+/n54+f97/iLVskGePIyvZpKi66ft4WB/R4tNB1VvCE7UrWF7QsY17AoZksarZP0nWSPQ3JKgUVm5w/70TAKuruxBfd7avfXuRvKPIt9B1FZQ7uUORYZy3X/woer4qcX7FNvE+Lm6xgPIvTh5gncTnio3VGBmAklZBYCxmCkZVaV1Y7Y2khQHO26QIXCec5Z+uawO8TzJNNgb9uFcBN9HmjW4r+Oed/Y74ezRYMizF4kgRUccA9sbXM25wX9yxbM95FWOhTec2Xj+efD01Y5xLDHVux5M8QRwl/xRNwiYjQIKgLz7MrD3jB+BPjZJnj+Rslhb1uotRrWWBbI9WsR38Aq7wYKgKiGdwfxY4UpqgLA9liQcB0Y9sAHPalD9dJp5p90BxXEQOD+jiFV7XadmEIJKwyBsEqE2ybxA8vDFt9NQs0GUia0tBdcFsEQakMdQBtHAQVgj31ddzJUgv5dkN7jQdu3UCw8BvQ3NRYxcF2q/FVt2Eb71Jo93FQjdTBXXg7hmXdbQ1awaO9XpYUOpl3YXTqVUXA9lwXFcwf60lxYLK9AYPULkxPam0OuDq9CQbT2gxUxbI8HwDHywuy+XEN+rlQS1smMotXv8cwbo2pjA8oOJTwGLBGQpjx+ibgRjlcM1AtRCeVdd5v06Qor8bL3BlfxVSe0lNJkNEmKlBVRgkgNW4bD6lfINg3ARNp2RG4ZwXDNBqW5Yx6gPIdkEDzGEJk4zZOwmUe7SwMbrTf3j8S2AZfoLHBBp+C7Nsf9SOlg4Wt3KICeZRYp2METajG8R34zYVasoKjBVJ+nJah6SiGm1otVXsrZMOsTB/ndqYGqhbeiqFotzNFBsqJkXICOzja5gcwG+qUdCDzBkL6REA5ZCo54D4SNizgu2sYq+/8bbn3aPlqDcWmxioRqe4VbTlJWt7aVg0xV9xSC6oU/yKFxl4Dd1I/gRKTJdTWQ6bSA9+LBfLBUkXI4qkjJOiefYAMOk47RIGNKahuoQznYxIeffMfyc0kRFS3dEczRKbKEJkqQwZEocEZITQ4IwirvnCrRqtBUcnpTfpTaw2pN6mg3x7P8q/yv5nyAIBNiho7MTVsCXSiDkUnaVB0ov5EDduT3X7xAvxM0O0vDYU9X/Oqy8gdUMXHxoYMgQFDvZHzjLPYonA004jd0e2v6pNdUwpiToHNKWxjitCYoeUUo9P4aKFZksX88MD4Ljm9+XZ6Z+94fDwe/wsgwABYhMLTwicAAA==$"
     
     optimizer = AstroidSolver()
-    optimizer.add_astroid_locations(astroid_location)
-    optimizer.run_solver()
+    optimizer.add_astroid_locations(np.array(astroid_location))
+    optimizer.run_solver(miner_timelimit=10.0, miner_gap=0.0, belt_timelimit=1.0, belt_gap=90.0)
     optimizer.save_variables("variables.txt")
     blueprint = optimizer.get_solution_blueprint(MINER_BLUEPRINT)
     print(blueprint)
