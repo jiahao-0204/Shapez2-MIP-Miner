445b8bfc90d1b480d57466f3d8f144ab73b40e68
Jiahao Wang <github@jiahao.wang>
2025-06-12 11:36:54 +0100

    fix(incorrect logging path)

--- server/start.sh
+++ server/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
 conda activate shapez2
-exec uvicorn app.webapp:app --host 0.0.0.0 --port 8000 --reload --log-config app/logging/logging_config.yaml
+exec uvicorn app.webapp:app --host 0.0.0.0 --port 8000 --reload --log-config app/custom_logging/logging_config.yaml
